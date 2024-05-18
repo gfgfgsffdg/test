2eb476c4c704743664e0d900923926582bbcc829
JayPoch <technobladedeutsch@gmail.com>
2024-05-19 00:30:32 +0200

    removed the old wetter.py beacuse i will only update the new one and added icons to the new one

--- wetterapi/wetter-new-modern.py
+++ wetterapi/wetter-new-modern.py
@@ -15,6 +15,22 @@ response_raw = None
 response_raw1 = []
 response_raw2 = []
 proccesed_condition = None
+icon_plaintext = None
+weather_icons = {
+    "clear-day": "☀️",  # Sun
+    "clear-night": "🌙",  # Moon
+    "partly-cloudy-day": "🌤️",  # Sun behind cloud
+    "partly-cloudy-night": "🌥️",  # Moon behind cloud
+    "cloudy": "☁️",  # Cloud
+    "fog": "🌫️",  # Fog
+    "wind": "🌬️",  # Wind face
+    "rain": "🌧️",  # Cloud with rain
+    "sleet": "🌨️",  # Cloud with snow and rain
+    "snow": "❄️",  # Snowflake
+    "hail": "🌩️",  # Cloud with hail
+    "thunderstorm": "⛈️",  # Cloud with lightning and rain
+    "null": "❓"  # Question mark
+}
 proccesed_condition_auf_deutsch = {
     "dry": "trocken",
     "fog": "Nebel",
@@ -45,6 +61,7 @@ def get_lat_and_long():
 def get_weather_ant_temp(lat, long):
     global proccesed_condition
     global temperatur
+    global icon_plaintext
     parameter["lat"] = lat
     parameter["lon"] = long
     response_raw = requests.get("https://api.brightsky.dev/current_weather", headers=header, params=parameter)
@@ -53,22 +70,24 @@ def get_weather_ant_temp(lat, long):
     response_raw2 = response_raw1['weather']
     proccesed_condition = response_raw2["condition"]
     temperatur = int(response_raw2["temperature"])
-    print(proccesed_condition)
-    print(temperatur)
+    icon_plaintext = response_raw2["icon"]
+
     #gui_update()
 def gui_update(): #customtkinter CTK
 
-    global wo, proccesed_condition, proccesed_condition_auf_deutsch, Das_wetter, die_temperatur, root
+    global wo, proccesed_condition, proccesed_condition_auf_deutsch, Das_wetter, die_temperatur, root, weather_icons, icon_plaintext, icon
     Das_wetter = customtkinter.CTkLabel(root, text="Das Wetter ist " + proccesed_condition_auf_deutsch[proccesed_condition], font=("arial", 20))
     Das_wetter.pack(pady=5)
     die_temperatur = customtkinter.CTkLabel(root, text="Die Temperatur beträgt " + str(temperatur) + "°", font=("arial", 20))
     die_temperatur.pack(pady=5)
-    welche_stadt_label = customtkinter.CTkLabel(root, text="Welche stadt?", font=("arial", 20))
+    welche_stadt_label = customtkinter.CTkLabel(root, text="Welche tadt?", font=("arial", 20))
     welche_stadt_label.place(x=5, y=80)
     wo = customtkinter.CTkEntry(root)
     wo.pack(pady=5)
     neu_laden = customtkinter.CTkButton(root, text="Neu laden", command=get_lat_and_long)
     neu_laden.pack(pady=15)
+    icon = customtkinter.CTkLabel(root, text=weather_icons[icon_plaintext], font=("arial", 50))
+    icon.place(x=10, y=5)
     root.mainloop()
 
 
