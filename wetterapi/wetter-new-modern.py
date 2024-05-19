b14cdbd6209b7408e74ad0b957110dbc99782ae6
JayPoch <technobladedeutsch@gmail.com>
2024-05-19 14:37:20 +0200

    made the icons into images  and fixed some bugs

--- wetterapi/wetter-new-modern.py
+++ wetterapi/wetter-new-modern.py
@@ -2,6 +2,7 @@ import requests
 import json
 import customtkinter
 from geopy.geocoders import Nominatim
+from PIL import Image
 #variables
 response_raw = None
 response_raw1 = []
@@ -17,19 +18,19 @@ response_raw2 = []
 proccesed_condition = None
 icon_plaintext = None
 weather_icons = {
-    "clear-day": "☀️",  # Sun
-    "clear-night": "🌙",  # Moon
-    "partly-cloudy-day": "🌤️",  # Sun behind cloud
-    "partly-cloudy-night": "🌥️",  # Moon behind cloud
-    "cloudy": "☁️",  # Cloud
-    "fog": "🌫️",  # Fog
-    "wind": "🌬️",  # Wind face
-    "rain": "🌧️",  # Cloud with rain
-    "sleet": "🌨️",  # Cloud with snow and rain
-    "snow": "❄️",  # Snowflake
-    "hail": "🌩️",  # Cloud with hail
-    "thunderstorm": "⛈️",  # Cloud with lightning and rain
-    "null": "❓"  # Question mark
+    "clear-day": customtkinter.CTkImage(dark_image=Image.open("clear-day.png"), size=(50, 50)),  # Sun
+    "clear-night": customtkinter.CTkImage(dark_image=Image.open("clearnight.png"), size=(50, 50)),  # Moon
+    "partly-cloudy-day": customtkinter.CTkImage(dark_image=Image.open("partly_cloudy_day.png"), size=(50, 50)),  # Sun behind cloud
+    "partly-cloudy-night": customtkinter.CTkImage(dark_image=Image.open("barely_cloudy_night.png"), size=(50, 50)),  # Moon behind cloud
+    "cloudy": customtkinter.CTkImage(dark_image=Image.open("cloudy.png"), size=(50, 50)),  # Cloud
+    "fog": customtkinter.CTkImage(dark_image=Image.open("fog.png"), size=(50, 50)),  # Fog
+    "wind": customtkinter.CTkImage(dark_image=Image.open("wind.png"), size=(50, 50)),  # Wind face
+    "rain": customtkinter.CTkImage(dark_image=Image.open("rain.png"), size=(50, 50)),  # Cloud with rain
+    "sleet": customtkinter.CTkImage(dark_image=Image.open("sleet.png"), size=(50, 50)),  # Cloud with snow and rain
+    "snow": customtkinter.CTkImage(dark_image=Image.open("rain.png"), size=(50, 50)),  # Snowflake
+    "hail": customtkinter.CTkImage(dark_image=Image.open("hail.png"), size=(50, 50)),  # Cloud with hail
+    "thunderstorm": customtkinter.CTkImage(dark_image=Image.open("thunderstorm.png"), size=(50, 50)),  # Cloud with lightning and rain
+    "null": customtkinter.CTkImage(dark_image=Image.open("null.png"), size=(50, 50))  # Question mark
 }
 proccesed_condition_auf_deutsch = {
     "dry": "trocken",
@@ -52,7 +53,7 @@ root.title("Wetter app")
 #functions
 def get_lat_and_long():
     global parameter
-    global error
+    global error, icon_plaintext, weather_icons, icon
     userinput = wo.get()
     location = geolocator.geocode(userinput)
     if location == None:
@@ -62,6 +63,7 @@ def get_lat_and_long():
         get_weather_ant_temp(str(location.latitude), str(location.longitude))
         Das_wetter.configure(text="Das Wetter ist " + proccesed_condition_auf_deutsch[proccesed_condition])
         die_temperatur.configure(text="Die Temperatur beträgt " + str(temperatur) + "°")
+        icon.configure(image=weather_icons[icon_plaintext])
 #main
 def get_weather_ant_temp(lat, long):
     global proccesed_condition
@@ -70,32 +72,28 @@ def get_weather_ant_temp(lat, long):
     parameter["lat"] = lat
     parameter["lon"] = long
     response_raw = requests.get("https://api.brightsky.dev/current_weather", headers=header, params=parameter)
-    #format the json and print at the end
+    #format the json 
     response_raw1 = response_raw.json()
     response_raw2 = response_raw1['weather']
     proccesed_condition = response_raw2["condition"]
     temperatur = int(response_raw2["temperature"])
     icon_plaintext = response_raw2["icon"]
     #gui_update()
-def gui_update(): #customtkinter CTK
-
-    global wo, proccesed_condition, proccesed_condition_auf_deutsch, Das_wetter, die_temperatur, root, weather_icons, icon_plaintext, icon, error
-    Das_wetter = customtkinter.CTkLabel(root, text="Das Wetter ist " + proccesed_condition_auf_deutsch[proccesed_condition], font=("arial", 20))
-    Das_wetter.pack(pady=5)
-    die_temperatur = customtkinter.CTkLabel(root, text="Die Temperatur beträgt " + str(temperatur) + "°", font=("arial", 20))
-    die_temperatur.pack(pady=5)
-    welche_stadt_label = customtkinter.CTkLabel(root, text="Welche tadt?", font=("arial", 20))
-    welche_stadt_label.place(x=5, y=80)
-    wo = customtkinter.CTkEntry(root)
-    wo.pack(pady=5)
-    neu_laden = customtkinter.CTkButton(root, text="Neu laden", command=get_lat_and_long)
-    neu_laden.pack(pady=15)
-    icon = customtkinter.CTkLabel(root, text=weather_icons[icon_plaintext], font=("arial", 50))
-    icon.place(x=10, y=5)
-    error = customtkinter.CTkLabel(root, text="", text_color="red")
-    error.pack()
-    root.mainloop()
 
 
 get_weather_ant_temp("52.52", "13.4")
-gui_update()
+Das_wetter = customtkinter.CTkLabel(root, text="Das Wetter ist " + proccesed_condition_auf_deutsch[proccesed_condition])
+Das_wetter.pack(pady=5)
+die_temperatur = customtkinter.CTkLabel(root, text="Die Temperatur beträgt " + str(temperatur) + "°", font=("arial", 20))
+die_temperatur.pack(pady=5)
+welche_stadt_label = customtkinter.CTkLabel(root, text="Welche tadt?", font=("arial", 20))
+welche_stadt_label.place(x=5, y=80)
+wo = customtkinter.CTkEntry(root)
+wo.pack(pady=5)
+neu_laden = customtkinter.CTkButton(root, text="Neu laden", command=get_lat_and_long)
+neu_laden.pack(pady=15)
+icon = customtkinter.CTkLabel(root, image=weather_icons[icon_plaintext], text="")
+icon.place(x=10, y=5)
+error = customtkinter.CTkLabel(root, text="", text_color="red")
+error.pack()
+root.mainloop()
